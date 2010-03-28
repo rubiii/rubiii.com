e3306b5cc9a382a7820a7d94edc95064ae11522c
rubiii <me@rubiii.com>
2010-03-28 19:20:35 +0200

    comments and cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,31 +3,43 @@ eval IO.read("/Users/rubiii/deploy.domainfactory")
 set :application, "rubiii"
 set :rails_env, :production
 
+# domainfactory
 set :deploy_to, "#{df_account_folder}/apps/#{application}"
 set :use_sudo, false
 
-default_run_options[:pty] = true
+# github
 set :scm, :git
 set :deploy_via, :remote_cache
 set :repository, "git@github.com:rubiii/rubiii.com.git"
+default_run_options[:pty] = true
 
-role :app, "rubiii.com"
-role :web, "rubiii.com"
-role :db, "rubiii.com", :primary => true
+# roles
+server "rubiii.com", :app, :web, :db, :primary => true
 
+# ssh
 set :user, df_ssh_username
 set :ssh_options, { :forward_agent => true }
 
+# passenger
 namespace :deploy do
   task :restart, :roles => :app do
     run "touch #{current_release}/tmp/restart.txt"
   end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
 end
 
+# database.yml
 after "deploy:update_code" do
-  database_configuration = IO.read("/Users/rubiii/database.domainfactory")
+  database_yml = IO.read("/Users/rubiii/database.domainfactory")
 
   run "mkdir -p #{deploy_to}/#{shared_dir}/config" 
-  put database_configuration, "#{deploy_to}/#{shared_dir}/config/database.yml" 
+  put database_yml, "#{deploy_to}/#{shared_dir}/config/database.yml" 
   run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml" 
 end
+
+# cleanup
+after "deploy", "deploy:cleanup"
