23c3dea11235c161a4a5f5aced56e4093b6ad243
rubiii <me@rubiii.com>
2010-03-28 19:05:23 +0200

    replace p tag inside h4 with strong

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,9 @@ $(function() {
 
 	/* animate project links and description */
 	$("h4 > a").mouseover(function() {
-	  $(this).next("p").animate({ marginLeft: "30px", paddingLeft: "15px" }, 500, "easeOutBack");
+	  $(this).next("strong").animate({ marginLeft: "30px", paddingLeft: "15px" }, 500, "easeOutBack");
   }).mouseout(function() {
-	  $(this).next("p").animate({ marginLeft: "15px", paddingLeft: "30px" }, 500, "easeOutBack");
+	  $(this).next("strong").animate({ marginLeft: "15px", paddingLeft: "30px" }, 500, "easeOutBack");
 	});
 
 });
