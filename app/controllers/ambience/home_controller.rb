17db3e753fe605d65700926fed23c0a677fb9d3e
rubiii <me@rubiii.com>
2010-03-28 23:04:05 +0200

    disabling cache for now

--- app/controllers/ambience/home_controller.rb
+++ app/controllers/ambience/home_controller.rb
@@ -1,6 +1,6 @@
 class Ambience::HomeController < ApplicationController
 
-  caches_action :index, :layout => false, :cache_path => cache_path(:ambience)
+  #caches_action :index, :layout => false, :cache_path => cache_path(:ambience)
 
   def index
     @articles = Article.all_by_category :ambience
