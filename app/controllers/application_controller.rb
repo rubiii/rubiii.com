2d6437a49aa794e8aff12b1f76ed49d0e4f99dfa
rubiii <me@rubiii.com>
2010-04-11 17:07:37 +0200

    removed unused cache method and constant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  CachePath = Rails.root.join("public", "cache")
-
   helper :all
   protect_from_forgery
-
-protected
-
-  def self.cache_path(subfolder = nil)
-    path = CachePath
-    path.join(subfolder.to_s) if subfolder
-    path.to_s
-  end
 
 end
